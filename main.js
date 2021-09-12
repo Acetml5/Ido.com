3cb7510f28b8c902f84f3ab7dc9bf326b88f75fd
Acetml5 <90198157+Acetml5@users.noreply.github.com>
2021-09-12 10:00:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function myFunction(){
+    window.location="My_Life.html";
+}
+function myFunction1(){
+    window.location="hobbies.html";
+}
+function myFunction2(){
+    window.location="Gifs.html";
+}
+function myFunction3(){
+    window.location="Videos.html";
+}
+function myFunction4(){
+    window.location="Inspiration.html";
+}
+function myFunction5(){
+    window.location="Contact.html";
+}
+function myFunction7(){
+    window.location="Projects.html";
+}
+function myFunction6(){
+    window.location="Index.html";
+}
